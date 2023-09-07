e2f965ff7d13cc4991859e72b7cac83f567eb814
Tuan T. Pham <tuan.pham@nalej.com>
2023-09-07 18:00:51 -0400

    Update Dockerfile and others
    Signed-off-by: Tuan T. Pham <tuan.pham@nalej.com>

--- backend/node_app/init/create_admins.sql
+++ backend/node_app/init/create_admins.sql
@@ -1,1 +1,12 @@
-INSERT INTO admins (username) VALUES ('007') ON CONFLICT DO NOTHING;
+-- \connect game_changer;
+CREATE TABLE IF NOT EXISTS admins (
+     id SERIAL,
+     username text NOT NULL
+ );
+
+-- INSERT INTO admins (username) VALUES ('007') ON CONFLICT DO NOTHING;
+
+INSERT INTO admins ("user_id", username) VALUES (7890123456, 'TEST.TEST.T.7890123456');
+
+CREATE USER postgres_agent;
+
