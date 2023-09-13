f390dc559c76f63103a99a9b48ec26488c4fca4d
Tuan T. Pham <tuan.pham@nalej.com>
2023-09-13 09:56:45 -0400

    WIP
    Signed-off-by: Tuan T. Pham <tuan.pham@nalej.com>

--- backend/node_app/init/create_admins.sql
+++ backend/node_app/init/create_admins.sql
@@ -4,9 +4,8 @@ CREATE TABLE IF NOT EXISTS admins (
      username text NOT NULL
  );
 
--- INSERT INTO admins (username) VALUES ('007') ON CONFLICT DO NOTHING;
+INSERT INTO admins (username) VALUES ('007') ON CONFLICT DO NOTHING;
 
 INSERT INTO admins ("user_id", username) VALUES (7890123456, 'TEST.TEST.T.7890123456');
 
 CREATE USER postgres_agent;
-
