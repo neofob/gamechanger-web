13084548ca9698b3f91ddc534ac35709f3e4076c
Tuan T. Pham <tuan.pham@nalej.com>
2023-09-19 19:32:44 -0400

    WIP
    Signed-off-by: Tuan T. Pham <tuan.pham@nalej.com>

--- backend/node_app/init/create_admins.sql
+++ backend/node_app/init/create_admins.sql
@@ -1,12 +1,10 @@
--- \connect game_changer;
-CREATE TABLE IF NOT EXISTS admins (
-     id SERIAL,
-     username text NOT NULL
- );
-
+\connect game_changer;
 INSERT INTO admins (username) VALUES ('007') ON CONFLICT DO NOTHING;
+INSERT INTO admins (id, username) VALUES (123456, 'TEST.TEST.T.7890123456');
 
-INSERT INTO admins ("user_id", username) VALUES (7890123456, 'TEST.TEST.T.7890123456');
-
-INSERT INTO users ("user_id", cn, first_name, last_name, extra_fields, is_super_admin) VALUES (7890123456, 'TEST.TEST.T.7890123456' ,'TEST','TEST', '{"gamechanger": {"is_beta": true, "is_admin": true, "is_internal": true},  "clones_visited": ["gamechanger"]}', TRUE);
-CREATE USER postgres_agent;
+INSERT INTO api_keys (username, active, "createdAt") VALUES ('007', 'true', '2023-03-30 00:00:00+9') ON CONFLICT DO NOTHING;
+INSERT INTO permissions (name, "createdAt", "updatedAt") VALUES ('Gamechanger Admin','2023-03-30 00:00:00+9','2023-03-30 00:00:00+9') ON CONFLICT DO NOTHING;
+INSERT INTO roles(name, "createdAt", "updatedAt", owner, application, product, sod_id, description) VALUES ('Gamechanger Admin', '2023-03-30 00:00:00+9', '2023-03-30 00:00:00+9', '007', 'test', 'test','test','test') ON CONFLICT DO NOTHING;
+INSERT INTO userroles(userid, roleid, "createdAt", "updatedAt") VALUES ('1', '1', '2023-03-30 00:00:00+9', '2023-03-30 00:00:00+9') ON CONFLICT DO NOTHING;
+INSERT INTO roleperms(roleid, permissionid, "createdAt", "updatedAt") VALUES ('1', '1', '2023-03-30 00:00:00+9', '2023-03-30 00:00:00+9') ON CONFLICT DO NOTHING;
+-- INSERT INTO gc_users (user_id, "createdAt", is_admin, cn) VALUES ('1', '2023-03-30 00:00:00+9', 'true', '007') ON CONFLICT DO NOTHING;
